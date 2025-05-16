eb691a9a6b6559f814c69886e10f5885a98e586d
Philip-Machar <philipmachar788@gmail.com>
2025-05-16 10:09:15 +0300

    prompt templates

---
+++ 2_promt_templates/2_promt_templates_starter.py
@@ -0,0 +1,28 @@
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+load_dotenv()
+
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    google_api_key=google_api_key,
+    temperature=0.7
+)
+
+#prompt template
+message = [("system", "you are a comedian who tells jokes about {topic}"), ("human", "tell me a joke about {joke}")]
+
+#converting prompt template into a form langchain can understand
+prompt_template = ChatPromptTemplate.from_messages(message)
+
+#populating the variables in the prompt and assigning the final prompt with everythig done to variable prompt
+prompt = prompt_template.invoke({"topic": "animals", "joke": "elephant"})
+
+#assigning ai response to results variable
+results = llm.invoke(prompt)
+
+#printing the ai response(content has the message itself)
+print(results.content)
